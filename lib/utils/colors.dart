5dbf52d5aeec5a72ef7cb0e20accdc13c40c7e6b
lonebhen <ben.angmortey@gmail.com>
2024-06-29 22:50:12 +0000

    colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryPurple = Colors.purple;
+  static const Color primaryWhite = Colors.white;
+  static const Color lavender = Color(0xFFE6E6FA);
+  static const Color lilac = Color(0xFFC8A2C8);
+  static const Color plum = Color(0xFFDDA0DD);
+  static const Color mauve = Color(0xFFE0B0FF);
+  static const Color amethyst = Color(0xFF9966CC);
+  static const Color silver = Color(0xFFC0C0C0);
+  static const Color lightGray = Color(0xFFD3D3D3);
+  static const Color darkGray = Color(0xFFA9A9A9);
+  static const Color pastelPink = Color(0xFFFFD1DC);
+  static const Color blush = Color(0xFFDE5D83);
+  static const Color softBlue = Color(0xFFADD8E6);
+  static const Color mintGreen = Color(0xFF98FF98);
+}
+
