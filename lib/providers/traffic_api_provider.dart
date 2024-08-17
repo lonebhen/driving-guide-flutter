38490e8c3b1ec13af0542d34cabd5b60c7fd43c7
lonebhen <ben.angmortey@gmail.com>
2024-08-17 21:46:17 +0000

    traffic screen

--- lib/providers/traffic_api_provider.dart
+++ lib/providers/traffic_api_provider.dart
@@ -1,9 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:driving_guide/providers/user_profile.dart';
 import 'package:http/http.dart' as http;
 
 class TrafficApiProvider {
-  static const String _baseUrl = 'http://192.168.43.240:5000/predict';
+  // static const String _baseUrl = 'http://192.168.43.240:5000/predict';
+
+  static const String _baseUrl = 'https://driving-guide.onrender.com';
+
+  UserProfile userProfile = new UserProfile();
 
   Future<String?> uploadImage(File image) async {
     try {
@@ -12,6 +17,13 @@ class TrafficApiProvider {
         Uri.parse(_baseUrl),
       );
       request.files.add(await http.MultipartFile.fromPath('file', image.path));
+
+      Map<String, dynamic> jsonMap = {
+        'local_dialect': userProfile.getUserDialect(),
+      };
+
+      request.fields['json'] = jsonEncode(jsonMap);
+
 
       final response = await request.send();
       if (response.statusCode == 200) {
--- lib/screens/traffic-prediction/traffic_screen.dart
+++ lib/screens/traffic-prediction/traffic_screen.dart
@@ -1,6 +1,8 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter_tts/flutter_tts.dart';
 import '../../providers/traffic_api_provider.dart';
 
@@ -13,6 +15,7 @@ class _TrafficScreenState extends State<TrafficScreen> {
   File? _image;
   final picker = ImagePicker();
   final FlutterTts flutterTts = FlutterTts();
+  AudioPlayer audioPlayer = AudioPlayer();
   final TrafficApiProvider apiProvider = TrafficApiProvider();
   bool _isLoading = false;
 
@@ -47,22 +50,30 @@ class _TrafficScreenState extends State<TrafficScreen> {
     }
   }
 
+  // Future<void> _playAudio(String audioUrl) async {
+  //   try {
+  //     await flutterTts.stop();
+  //     await flutterTts.setLanguage("en-US");
+  //     await flutterTts.setSpeechRate(1.0);
+  //     await flutterTts.speak(Uri.play(audioUrl)); // check here again, it was play
+  //   } catch (e) {
+  //     print('Failed to play audio: $e');
+  //   }
+  // }
+
   Future<void> _playAudio(String audioUrl) async {
-    try {
-      await flutterTts.stop();
-      await flutterTts.setLanguage("en-US");
-      await flutterTts.setSpeechRate(1.0);
-      await flutterTts.speak(Uri.decodeFull(audioUrl)); // check here again, it was play
-    } catch (e) {
-      print('Failed to play audio: $e');
-    }
+      await audioPlayer.stop(); // Stop any currently playing audio
+      await audioPlayer.play(audioUrl as Source); // Play the audio from URL
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Upload Traffic Sign Picture'),
+        backgroundColor: Colors.purple,
       ),
       body: Center(
         child: _isLoading
